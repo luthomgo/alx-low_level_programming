59c65c1c157c97cd733ab0b3812181c59f26b750
luthomgo <lutho.usa@gmail.com>
2023-06-15 10:25:20 -0700

    Task4:4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ * main - C program that prints
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n")
+
+		return(0)
+}
