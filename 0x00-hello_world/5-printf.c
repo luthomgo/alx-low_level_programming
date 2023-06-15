d71f04d55cb382f40b98b8a41d4ddd19ad7e5ae2
luthomgo <lutho.usa@gmail.com>
2023-06-15 11:16:08 -0700

    Task 5: 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints a program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piesce of art,\n");
+	return (0);
+}
