4cd84247687a061fad45ab9901ddeda353b8c080
luthomgo <lutho.usa@gmail.com>
2023-06-17 11:35:01 -0700

    Task 0:0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-		printf("%d zero\n", n);
+		printf("%d negative\n", n);
 
 	return (0);
 }
