11e8a9f6ea1c0a6b44a7291581eeed68e3dce493
luthomgo <lutho.usa@gmail.com>
2023-06-18 06:08:39 -0700

    Task 4:4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - program that prints the ALPHA in lowercase except q and e
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 97; b <= 122; b++)
+	{
+	if (b == 113 && b == 101)
+		continue;
+	putchar(b);
+	}
+	putchar ('\n');
+	return (0);
+}
+
