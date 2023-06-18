426ff2bfd879e0eb954b292dc8dcd4a2801ff70c
luthomgo <lutho.usa@gmail.com>
2023-06-18 05:21:34 -0700

    Task 2:2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - program the the ALPHA in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 97; b <= 122; b++)
+		putchar(b);
+	putchar('\n');
+
+	return (0);
+}
