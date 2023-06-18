58155682e54056a5a109b60ca6ab5118037d4d68
luthomgo <lutho.usa@gmail.com>
2023-06-18 09:09:59 -0700

    Task 9: 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - program that prints all possible combinations
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+	putchar(',');
+
+	return (0);
+}
