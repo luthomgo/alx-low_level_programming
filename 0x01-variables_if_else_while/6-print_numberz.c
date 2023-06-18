4c6fd1e8a84a64f123fc395068ca5dbc23f38eab
luthomgo <lutho.usa@gmail.com>
2023-06-18 06:49:26 -0700

    Task 6:6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - program that prints single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
