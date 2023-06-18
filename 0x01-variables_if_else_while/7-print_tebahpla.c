0dca94c0deab4085344ed36b18926cc50d363d2e
luthomgo <lutho.usa@gmail.com>
2023-06-18 07:23:03 -0700

    Task 7:7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main- program that prints the lowercase ALPHA in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a <= 97; a--)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
