23a51fa7d1481804feb68c4d6de5de4e9cfbb391
luthomgo <lutho.usa@gmail.com>
2023-06-20 12:14:42 -0700

    Task 5: 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * print_sign - function that prints the sign of a num
+ * @n: the number
+ *
+ * Return: 1 that prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if the n is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(0);
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
