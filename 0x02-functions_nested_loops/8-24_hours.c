3e9dba2e9e74dbe0306db655d2794fc6dd064822
luthomgo <lutho.usa@gmail.com>
2023-06-20 16:58:38 -0700

    Task 8:8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * jack_bauer - function that prints every minute of the day
+ *
+ * Return: Alawys 0
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(':');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
