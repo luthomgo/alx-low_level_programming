bf6e8b1c9eaac1201341d3261a16ff24c27a43aa
luthomgo <lutho.usa@gmail.com>
2023-06-20 09:18:48 -0700

    Task 2:2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - function that prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - function that print 10 times the alphabet, in lowercase
  *
  * Return: Always 0
  */
@@ -8,15 +8,18 @@ void print_alphabet_x10(void)
 {
 	char alpha;
 	int a;
-	
-	while (a = 0; a < 10)
+
+	a = 0;
+	alpha = 97;
+
+	while (a < 10)
 	{
-		while (alpha = 97; alpha <= 122)
+		while (alpha <= 122)
 		{
-			putchar(alpha)
-				alpha++;
+			putchar(alpha);
+			alpha++;
 		}
-		putchar('\n')
-			a++;
+		putchar('\n');
+		a++;
 	}
 }
