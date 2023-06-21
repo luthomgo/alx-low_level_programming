77135e814b1e3e894f734a03e40c1ff523897efd
luthomgo <lutho.usa@gmail.com>
2023-06-21 07:16:44 -0700

    Task 16: 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - program that finds and prints the first 98 num start with 1 and 2
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	unsigned long int b;
+	unsigned long int c;
+	unsigned long int d;
+
+	printf("%lu, %lu", b, c);
+
+	for (a = 0; a < 96; a++)
+	{
+		d = b + c;
+		printf(", %lu", d);
+		b = c;
+		c = d;
+	}
+	printf("\n");
+	return (0);
+}
