29c9d7f47e2a10375c9efa40d1efd740ea14ad7e
luthomgo <lutho.usa@gmail.com>
2023-06-21 04:34:25 -0700

    Task 13: 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	int a;
+	int sum;
 
 	sum = 0;
 
-	for (a = 0; a < 1024, a++)
+	for (a = 0; a < 1024; a++)
 	{
 		if ((a % 3) == 0 || (a % 5) == 0)
 			sum = sum + a;
 	}
-	printf("%d\n", sum)
+	printf("%d\n", sum);
 		return (0);
 }
