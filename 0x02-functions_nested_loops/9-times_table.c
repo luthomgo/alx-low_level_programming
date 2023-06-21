2525bb4f3124fcac5678927925f66697f16aa9ce
luthomgo <lutho.usa@gmail.com>
2023-06-20 17:23:58 -0700

    Task 9: 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * times_table - function that prints the 9 times tables, starting with 0
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		b = 1 * 9;
+
+		if (b > 9)
+			putchar((b / 10) + '0');
+		if (a < 9)
+			putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+}
