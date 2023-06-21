efcea72a8d5aee9fa2f9863a236022b6077b0511
luthomgo <lutho.usa@gmail.com>
2023-06-21 06:52:23 -0700

    Task 15: 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - seq that is generate by adding the two terms start with 1 and 2
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	unsigned long int b;
+	unsigned long int c;
+	unsigned long int d;
+	unsigned long int sum;
+
+	b = 1;
+	c = 2;
+	sum = 0;
+
+	while (b <= 4000000)
+	{
+		if (b % 2 == 0)
+			sum = sum + b;
+		result = b + c;
+		b = c;
+		c = d;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
