c5a49960aeb4b8fc1e06eeaf2f6d53054f9ce780
luthomgo <lutho.usa@gmail.com>
2023-06-20 18:15:46 -0700

    Task 10: 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * add - function that adds two int and return the result
+ * @a: first int
+ * @b: sec int
+ * Return: result of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
