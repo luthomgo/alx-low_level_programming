66fcc6377675752c158c9cc99aaa529614addc3a
luthomgo <lutho.usa@gmail.com>
2023-06-22 13:28:46 -0700

    Task 3: 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_number - function that prints the number from 0 to 9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
