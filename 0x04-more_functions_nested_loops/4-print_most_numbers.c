790bfbdc15e0de90b9dc558433a327b68afb1925
luthomgo <lutho.usa@gmail.com>
2023-06-22 14:13:37 -0700

    Task 4: 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - func that prints the num from 0 to 9 not print 2 and 4
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50 && a != 52)
+			break;
+		_putchar(a);
+	}
+	_putchar('\n');
+}
