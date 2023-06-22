3b4c62f05a377c8412fc82cf8a7a53bee706d941
luthomgo <lutho.usa@gmail.com>
2023-06-22 16:28:01 -0700

    Task 6: 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - a function that draws a diagonal line
+ * @n: is the num of times the char \ should be printed
+ *
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+	}
+	_putchar('\n');
+}
