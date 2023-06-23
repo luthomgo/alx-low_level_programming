49f925ecb02fc86061cdf51004a292041563df18
luthomgo <lutho.usa@gmail.com>
2023-06-23 09:37:59 -0700

    task 12: 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,14 @@
  */
 void print_number(int n)
 {
-	unsigned int a;
-
 	if (n < 0)
 	{
-		_putchar (45);
-		a = -n;
+		_putchar(45);
+		n = -n;
 	}
-	if (a / 10)
+	if (n / 10)
 	{
-		print_number(a / 10);
+		print_number(n / 10);
 	}
-	_putchar((a % 10) + 48);
+	_putchar((n % 10) + 48);
 }
