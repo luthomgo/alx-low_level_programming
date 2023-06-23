a22d4d345ad310b8570370e3794503f9a08d9df7
luthomgo <lutho.usa@gmail.com>
2023-06-23 12:34:46 -0700

    Task 12:101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * print_number - function that prints an integer
- * @n: int that need to checked
+ * print_number- function that prints an integer
+ * @n: int used
  *
  * Return: Always 0
  */
