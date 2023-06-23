41ac7fd9e18368ae354710f9b813b790a1ac4ecc
luthomgo <lutho.usa@gmail.com>
2023-06-23 15:31:18 -0700

    task 2:2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,4 +9,17 @@
  */
 int largest_number(int a, int b, int c)
 {
-
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
