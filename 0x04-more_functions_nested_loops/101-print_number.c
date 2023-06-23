1c5c95064ae804a0c53799dfd0548b21dfb27cbe
luthomgo <lutho.usa@gmail.com>
2023-06-23 09:12:58 -0700

    task 12:101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "main.h"
 /**
  * print_number - function that prints an integer
  * @n: int that need to checked
@@ -6,3 +7,30 @@
  * Return: Always 1
  */
 void print_number(int n)
+{
+	unsigned int a;
+	unsigned int b;
+	unsigned int c;
+
+	if (n < 0)
+	{
+		_putchar (45);
+		a = n * -1;
+	}
+	else
+	{
+		a = n;
+	}
+	b = a;
+	c = 1;
+
+	while (b > 9)
+	{
+		b = b / 10;
+		c = c * 10;
+	}
+	for (; c >= 1; c = c / 10)
+	{
+		putchar(((a / c) % 10) + 48);
+	}
+}
