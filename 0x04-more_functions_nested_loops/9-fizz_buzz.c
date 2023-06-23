fa813bb7513ed1c1ce0461f59d7ff9e175ec32df
luthomgo <lutho.usa@gmail.com>
2023-06-22 18:19:22 -0700

    Task 9:9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 		{
 			printf("FizzBuzz");
 		}
-		else if (a == 3)
+		else if (a % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (a == 5)
+		else if (a % 5 == 0)
 		{
 			printf("Buzz");
 		}
