d86378633de9803e29e6a63a78d6910b13accda5
luthomgo <lutho.usa@gmail.com>
2023-06-26 11:17:45 -0700

    Task 11:101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - a program that generates random valid passwords for a program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	char password[11];
+
+	srand(time(NULL));
+	for (i = 0; i < 10; i++)
+	{
+		int r = rand() % 62;
+
+		if (r < 26)
+		{
+			password[i] = 'a' + r;
+		}
+		else if (r < 52)
+		{
+			password[i] = 'A' + r - 26;
+		}
+		else
+		{
+			password[i] = '0' + r - 52;
+		}
+	}
+	password[10] = '\0';
+	printf("%s\n", password);
+	return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The project is based on pointers,arrays and strings in c programming
