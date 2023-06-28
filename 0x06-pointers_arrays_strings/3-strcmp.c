b2dcf235cb4319528a4259446ceeda61dd81fb00
luthomgo <lutho.usa@gmail.com>
2023-06-28 08:29:20 -0700

    Task 3: 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * *_strncpy  - function that copies a string
+ * @dest:  a  string
+ * @src: a string
+ * @n:  integer
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	for (; a < n; a++)
+	{
+		dest[a] = '\0';
+	}
+	return (dest);
+}
