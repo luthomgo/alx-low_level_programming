6803ba37796c8d78979dfcab67324ccf3aff945f
luthomgo <lutho.usa@gmail.com>
2023-06-28 07:18:50 -0700

    Task 1:1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strncat - function that apppend two string
+ * @dest: first string
+ * @src: second string
+ * @n: a integer
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+		a++;
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		dest[a + b] = src[b];
+	}
+	dest[a + n] = '\0';
+	return (dest);
+}
