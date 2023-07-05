afa440b1c14d862c2870188854413c8b0a75bb76
luthomgo <lutho.usa@gmail.com>
2023-07-05 06:45:32 -0700

    Task 1: 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: pointer to a string
+ *
+ * Return: Always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
