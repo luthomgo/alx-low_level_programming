e3523a1a499a2fe2e66b63400a60c178d5c7f3b1
luthomgo <lutho.usa@gmail.com>
2023-07-19 18:56:33 -0700

    Task 4: 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the opcodes of its own main function
+ * @argc: integer
+ * @argv: array of pointer
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char *c = (char *)main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	a = atoi(argv[1]);
+
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (b = 0; b < a; b++)
+	{
+		opcode = *(unsigned char *)c;
+	printf("%.2x ", opcode);
+	if (b == a - 1)
+		continue;
+	printf(" ");
+	c++;
+	}
+	printf("\n");
+
+	return (0);
+}
